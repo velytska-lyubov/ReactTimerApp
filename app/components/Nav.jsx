f9c99faec95b68af59290402732ba8a82b884c46
velytska-lyubov <velytska.lyubov@gmail.com>
2017-02-18 23:10:33 +0100

    Initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,25 @@
+var React = require('react');
+var { Link, IndexLink } = require('react-router');
+
+const Nav = () => (
+  <div className="top-bar">
+    <div className="top-bar-left">
+      <ul className="menu">
+        <li className="menu-text">React Timer App</li>
+        <li>
+          <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink>
+        </li>
+        <li>
+          <Link to="/countdown" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Countdown</Link>
+        </li>
+      </ul>
+    </div>
+    <div className="top-bar-right">
+      <ul>
+        <li className="menu-text">Created by <a>Lyubov Velytska</a></li>
+      </ul>
+    </div>
+  </div>
+);
+
+module.exports = Nav;
